eedcd5d85758169c2cbb0acb09c84554359d2e63
Abhijeet Antin <abhijeet14antin@yahoo.com>
2023-12-27 17:19:47 -0800

    change name of variable in process generator

--- ProcessGenerator.cpp
+++ ProcessGenerator.cpp
@@ -15,18 +15,18 @@
  */
 std::vector<ProcessInfo> generateRandomProcesses()
 {
-	uint32_t n = 100;						// Number of processes
+	uint32_t n = 200;						// Number of processes
 	uint32_t minArrivalTime = 1;			// Earliest possible arrival
 	uint32_t maxArrivalTime = n * 2;		// Latest possible arrival
-	uint32_t minExecuteTime = 1;			// Minimum time to execute
-	uint32_t maxExecuteTime = n / 2 ;		// Maximum time to execute
+	uint32_t minBurstTime = 1;			// Minimum time to execute
+	uint32_t maxBurstTime = n / 2 ;		// Maximum time to execute
 
 	std::vector<ProcessInfo> list(n);
 	std::default_random_engine generator;
 	std::uniform_int_distribution<uint32_t> arrivalDistribution(minArrivalTime,
 		maxArrivalTime);
-	std::uniform_int_distribution<uint32_t> executeDistribution(minExecuteTime,
-		maxExecuteTime);
+	std::uniform_int_distribution<uint32_t> executeDistribution(minBurstTime,
+		maxBurstTime);
 
 	for (uint32_t i = 0; i < n; i++) {
 		list[i].processID = i + 1;
